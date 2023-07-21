03f80a96bb82f8231aa030e243107eb3b322d653
yastraeus <98371598+yastraeus@users.noreply.github.com>
2023-07-21 11:49:58 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -6,16 +6,16 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    //{ title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
   ],
 
   // 英雄区(首页顶部大卡)
   HERO_TITLE_1: '分享编程',
   HERO_TITLE_2: '与思维认知',
-  HERO_TITLE_3: 'TANGLY1024.COM',
-  HERO_TITLE_4: '新版上线',
-  HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HERO_TITLE_LINK: 'https://tangly1024.com',
+  HERO_TITLE_3: '',//TANGLY1024.COM
+  HERO_TITLE_4: '',//新版上线
+  HERO_TITLE_5: '',//NotionNext4.0 轻松定制主题
+  HERO_TITLE_LINK: 'https://tangly1024.com',//
 
   // 英雄区显示三个置顶分类
   HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
