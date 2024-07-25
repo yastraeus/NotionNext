5371431b3f7c6ce35434840682b5646e1c937b91
yastraeus <98371598+yastraeus@users.noreply.github.com>
2024-07-25 12:55:44 +0800

    Update config.js

--- themes/matery/config.js
+++ themes/matery/config.js
@@ -7,21 +7,21 @@ const CONFIG = {
   MATERY_HOME_NAV_BACKGROUND_IMG_FIXED: false, // 首页背景图滚动时是否固定，true 则滚动时图片不懂； false则随鼠标滚动
 
   // 是否显示开始阅读按钮
-  MATERY_SHOW_START_READING: true,
+  MATERY_SHOW_START_READING: false,
 
   // 菜单配置
-  MATERY_MENU_CATEGORY: true, // 显示分类
-  MATERY_MENU_TAG: true, // 显示标签
-  MATERY_MENU_ARCHIVE: true, // 显示归档
+  MATERY_MENU_CATEGORY: false, // 显示分类
+  MATERY_MENU_TAG: false, // 显示标签
+  MATERY_MENU_ARCHIVE: false, // 显示归档
   MATERY_MENU_SEARCH: true, // 显示搜索
 
   MATERY_POST_LIST_COVER: true, // 文章封面
   MATERY_POST_LIST_SUMMARY: true, // 文章摘要
   MATERY_POST_LIST_PREVIEW: true, // 读取文章预览
 
-  MATERY_ARTICLE_ADJACENT: true, // 显示上一篇下一篇文章推荐
-  MATERY_ARTICLE_COPYRIGHT: true, // 显示文章版权声明
-  MATERY_ARTICLE_RECOMMEND: true, // 文章关联推荐
+  MATERY_ARTICLE_ADJACENT: false, // 显示上一篇下一篇文章推荐
+  MATERY_ARTICLE_COPYRIGHT: false, // 显示文章版权声明
+  MATERY_ARTICLE_RECOMMEND: false, // 文章关联推荐
 
   MATERY_WIDGET_LATEST_POSTS: true, // 显示最新文章卡
   MATERY_WIDGET_ANALYTICS: false, // 显示统计卡
